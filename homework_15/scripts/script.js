39bb29112580b1b98cdfab707eb4da7427e9a40a
Yaroslav Krasatyuk <krasatuk@gmail.com>
2019-12-26 22:48:11 +0200

    Реализация Object.create

--- homework_15/scripts/script.js
+++ homework_15/scripts/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// 1. Реализовать цепочку протитопов с помощью Object.create.
+const objA = {
+    number: 2,
+    summ() {
+        return `${this.number} = Summ() is - ${this.number + this.number}`;
+    }
+};
+
+const objB = Object.create(objA);
+    objB.number = 4;
+    objB.mult = function() {
+        return `${this.number} = Mult() is - ${this.number * this.number}`;
+    };
+
+const objC = Object.create(objB);
+    objC.number = 6;
+    objC.increm = function() {
+        return `${this.number} = Increm() is - ${this.number + 1}`;
+    };
+
+console.log('Реализовать цепочку протитопов с помощью Object.create.');
+console.log(objA, objA.number, objA.summ());
+console.log(objB, objB.__proto__, objB.number, objB.summ(), objB.mult());
+console.log(objC, objC.__proto__, objC.number, objC.summ(), objC.mult(), objC.increm());
