66ac9fd926afd1bf0eac238400003bdd89109cad
Yaroslav Krasatyuk <krasatuk@gmail.com>
2020-02-02 12:38:11 +0200

    -добавил методы пересчета и подтверждения

--- homework_16/scripts/script.js
+++ homework_16/scripts/script.js
@@ -2,69 +2,93 @@
 
 class Hamburger {
 
-    static SIZE_SMALL = {
-        prise: 50, 
+    static SISE_SMALL = {
+        price: 50, 
         calories: 20,
     }
 
-    static SIZE_BIG = {
-        prise: 100, 
+    static SISE_BIG = {
+        price: 100, 
         calories: 40,
     }
 
     static FILLING_CHEESE = {
-        prise: 10, 
+        price: 10, 
         calories: 20,
     }
 
     static FILLING_LARD = {
-        prise: 20, 
+        price: 20, 
         calories: 5,
     }
 
     static FILLING_POTATO = {
-        prise: 15, 
+        price: 15, 
         calories: 10,
     }
 
     static ADDICTIVES_FLAVOR = {
-        prise: 15, 
+        price: 15, 
         calories: 0,
     }
 
     static ADDICTIVES_MAYONNAiSE = {
-        prise: 20, 
+        price: 20, 
         calories: 5,
     }
 
+    _prices = [];
+    _calories = [];
+
     constructor(sise, filling) {
-        this.prise = sise.prise + filling.prise;
+        this.price = sise.price + filling.price;
         this.calories = sise.calories + filling.calories;
     }
 
     addAdditives(additive) {
-        this.prise = this.prise + additive.prise;
-        this.calories = this.calories + additive.calories;
+        this._prices.push(additive.price);
+        this._calories.push(additive.calories);
     }
 
     addFillings(filling) {
-        this.prise = this.prise + filling.prise;
-        this.calories = this.calories + filling.calories;
+        this._prices.push(filling.price);
+        this._calories.push(filling.calories);
+    }
+
+    calculateCalories() {
+        return this._calories.reduce((acc, calorie) => {
+            acc += calorie;
+            return acc;
+        }, this.calories);
+    }
+
+    calculatePrice() {
+        return this._prices.reduce((acc, price) => {            
+            acc += price;
+            return acc;
+        }, this.price);
+    }
+
+    confirmOrder() {
+        this.price = this.calculatePrice();
+        this.calories = this.calculateCalories();
     }
 }
 
 console.log('Big Hamburger');
 
-let bigHamburger = new Hamburger(Hamburger.SIZE_BIG, Hamburger.FILLING_CHEESE);
+let bigHamburger = new Hamburger(Hamburger.SISE_BIG, Hamburger.FILLING_CHEESE);
 console.log(bigHamburger);
 
 bigHamburger.addAdditives(Hamburger.ADDICTIVES_MAYONNAiSE);
+console.log("Calories: " + bigHamburger.calculateCalories())
+console.log("Price: " + bigHamburger.calculatePrice());
+
+bigHamburger.addAdditives(Hamburger.ADDICTIVES_FLAVOR);
+console.log("Calories: " + bigHamburger.calculateCalories())
+console.log("Price: " + bigHamburger.calculatePrice());
 console.log(bigHamburger);
 
-console.log('Small Hamburger');
-let smallHamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.FILLING_POTATO);
-console.log(smallHamburger);
-
-smallHamburger.addAdditives(Hamburger.ADDICTIVES_FLAVOR);
-smallHamburger.addAdditives(Hamburger.ADDICTIVES_MAYONNAiSE);
-console.log(smallHamburger);
+console.log('Confirm!')
+bigHamburger.confirmOrder();
+console.log(bigHamburger);
