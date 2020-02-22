0c27dfb410b1895765c84b82eed6d1d3c921d8d5
Yaroslav Krasatyuk <krasatuk@gmail.com>
2020-02-22 14:04:04 +0200

    update - добавил методы на прототив - переделал метод для добавления Attendance  студента

--- homework_18/scripts/script.js
+++ homework_18/scripts/script.js
@@ -1,93 +1,93 @@
 var SIZE_SMALL = {
-    price: 50,
-    calories: 20,
-}
+  price: 50,
+  calories: 20
+};
 
 var SIZE_BIG = {
-    price: 100,
-    calories: 40,
-}
+  price: 100,
+  calories: 40
+};
 
 var FILLING_CHEESE = {
-    price: 10,
-    calories: 20,
-}
+  price: 10,
+  calories: 20
+};
 
 var FILLING_LARD = {
-    price: 20,
-    calories: 5,
-}
-  
+  price: 20,
+  calories: 5
+};
+
 var FILLING_POTATO = {
-    price: 15,
-    calories: 10,
-}
+  price: 15,
+  calories: 10
+};
 
 var ADDICTIVES_FLAVOR = {
-    price: 15,
-    calories: 0,
-}
+  price: 15,
+  calories: 0
+};
 
 var ADDICTIVES_MAYONNAiSE = {
-    price: 20,
-    calories: 5,
-}
+  price: 20,
+  calories: 5
+};
 
-function HamburgerMethods() {
-    this.addAdditives = function(filling) {
-        this._prices.push(filling.price);
-        this._calories.push(filling.calories);
-    }
+function HamburgerMethods() {}
 
-    this.addFillings = function(additive) {
-        this._prices.push(additive.price);
-        this._calories.push(additive.calories);
-    }
+HamburgerMethods.prototype.addAdditives = function(filling) {
+  this._prices.push(filling.price);
+  this._calories.push(filling.calories);
+};
 
-     this.calculateCalories = function() {
-        return this._calories.reduce((acc, calorie) => {
-            acc += calorie;
-            return acc;
-        }, this.calories);
-    }
+HamburgerMethods.prototype.addFillings = function(additive) {
+  this._prices.push(additive.price);
+  this._calories.push(additive.calories);
+};
 
-    this.calculatePrice = function() {
-        return this._prices.reduce((acc, price) => {            
-            acc += price;
-            return acc;
-        }, this.price);
-    }
+HamburgerMethods.prototype.calculateCalories = function() {
+  return this._calories.reduce((acc, calorie) => {
+    acc += calorie;
+    return acc;
+  }, this.calories);
+};
 
-    this.confirmOrder = function() {
-        this.price = this.calculatePrice();
-        this.calories = this.calculateCalories();
-    }
-}
+HamburgerMethods.prototype.calculatePrice = function() {
+  return this._prices.reduce((acc, price) => {
+    acc += price;
+    return acc;
+  }, this.price);
+};
+
+HamburgerMethods.prototype.confirmOrder = function() {
+  this.price = this.calculatePrice();
+  this.calories = this.calculateCalories();
+};
 
 function Hamburger(sise, filling) {
-    HamburgerMethods.apply(this, arguments);
-    this._prices = [];
-    this._calories = [];
-    this.price = sise.price + filling.price;
-    this.calories = sise.calories + filling.calories;
+  HamburgerMethods.apply(this, arguments);
+  this._prices = [];
+  this._calories = [];
+  this.price = sise.price + filling.price;
+  this.calories = sise.calories + filling.calories;
 }
 
 Hamburger.prototype = Object.create(HamburgerMethods.prototype);
 Hamburger.prototype.constructor = HamburgerMethods;
 
-console.log('Big Hamburger');
+console.log("Big Hamburger");
 let bigHamburger = new Hamburger(SIZE_BIG, FILLING_CHEESE);
 console.log(bigHamburger);
 
 bigHamburger.addAdditives(ADDICTIVES_MAYONNAiSE);
-console.log("Calories: " + bigHamburger.calculateCalories())
+console.log("Calories: " + bigHamburger.calculateCalories());
 console.log("Price: " + bigHamburger.calculatePrice());
 
 bigHamburger.addAdditives(ADDICTIVES_FLAVOR);
-console.log("Calories: " + bigHamburger.calculateCalories())
+console.log("Calories: " + bigHamburger.calculateCalories());
 console.log("Price: " + bigHamburger.calculatePrice());
 console.log(bigHamburger);
 
-console.log('Confirm!')
+console.log("Confirm!");
 bigHamburger.confirmOrder();
 console.log(bigHamburger);
--- homework_18/scripts/student.js
+++ homework_18/scripts/student.js
@@ -1,92 +1,83 @@
-'use strict';
+"use strict";
 
 class Student {
-    constructor(name, surname, year_of_birth) {
-        this.name = name;
-        this.surname = surname;
-        this.year_of_birth = year_of_birth;
-        this.rating = [8.2, 9.7, 7.8, 10, 9.2, 8.3, 9.1, 10, 8.0]
-        this.array = new Array(25);
-    }
+  constructor(name, surname, year_of_birth) {
+    this.name = name;
+    this.surname = surname;
+    this.year_of_birth = year_of_birth;
+    this.rating = [8.2, 9.7, 7.8, 10, 9.2, 8.3, 9.1, 10, 8.0];
+    this.attendance_array = [];
+  }
 
-    get age() {
-        return new Date().getFullYear() - this.year_of_birth;
-    }
+  get age() {
+    return new Date().getFullYear() - this.year_of_birth;
+  }
 
-    get averageRating() {
-        return +(this.rating.reduce((result, element) => {
-            return result + element;
-        }, 0) / this.rating.length).toFixed(2);
-    }
+  get averageRating() {
+    return +(
+      this.rating.reduce((result, element) => {
+        return result + element;
+      }, 0) / this.rating.length
+    ).toFixed(2);
+  }
 
-    get averageVisit() {
-        return +(this.array.reduce((result, element) => {            
-            return result + (+element);
-        }, 0) / this.array.length).toFixed(2);
-    }
+  get averageVisit() {
+    return +(
+      this.attendance_array.reduce((result, element) => {
+        return result + +element;
+      }, 0) / this.attendance_array.length
+    ).toFixed(2);
+  }
 
-    present() {
-        for (let index = 0; index < this.array.length; index++) {
-            const element = this.array[index];
-            if (element === undefined || element === null) {
-                this.array.splice(index, 1, true)
-                break;
-            }
-        }
-    }
-    
-    absent() {
-        for (let index = 0; index < this.array.length; index++) {
-            const element = this.array[index];
-            if (element === undefined || element === null) {
-                this.array.splice(index, 1, false)
-                break;
-            }
-        }
-    }
+  updateAttendance(key) {
+    const isAttended = key === "yes";
 
-    summary() {
-        if (this.averageRating > 9 && this.averageVisit > 0.9) {
-            return 'Ути какой молодчинка!';
-        } else if (this.averageRating < 9 && this.averageVisit < 0.9) {
-            return 'Редиска!';
-        } else {
-            return 'Норм, но можно лучше';
-        }
+    if (this.attendance_array.length <= 25) {
+      this.attendance_array.push(isAttended);
     }
+  }
+
+  summary() {
+    if (this.averageRating > 9 && this.averageVisit > 0.9) {
+      return "Ути какой молодчинка!";
+    } else if (this.averageRating < 9 && this.averageVisit < 0.9) {
+      return "Редиска!";
+    } else {
+      return "Норм, но можно лучше";
+    }
+  }
 }
 
-const student = new Student('Vava', 'Rossen', 1990);
+const student = new Student("Vava", "Rossen", 1990);
 
-student.absent(); //1
-student.present(); //2
-student.present(); //3
-student.present(); //4
-student.present(); //5
-student.present(); //6
-student.present(); //7
-student.present(); //8
-student.present(); //9
-student.present(); //10
-student.present(); //11
-student.present(); //12
-student.absent(); //13
-student.present(); //14
-student.present(); //15
-student.present(); //16
-student.present(); //17
-student.present(); //18
-student.present(); //19
-student.present(); //20
-student.present(); //21
-student.present(); //22
-student.present(); //23
-student.present(); //24
-student.present(); //25
+student.updateAttendance("no"); //1
+student.updateAttendance("yes"); //2
+student.updateAttendance("yes"); //3
+student.updateAttendance("yes"); //4
+student.updateAttendance("yes"); //5
+student.updateAttendance("yes"); //6
+student.updateAttendance("yes"); //7
+student.updateAttendance("yes"); //8
+student.updateAttendance("yes"); //9
+student.updateAttendance("yes"); //10
+student.updateAttendance("yes"); //11
+student.updateAttendance("yes"); //12
+student.updateAttendance("no"); //13
+student.updateAttendance("yes"); //14
+student.updateAttendance("yes"); //15
+student.updateAttendance("yes"); //16
+student.updateAttendance("yes"); //17
+student.updateAttendance("yes"); //18
+student.updateAttendance("yes"); //19
+student.updateAttendance("yes"); //20
+student.updateAttendance("yes"); //21
+student.updateAttendance("yes"); //22
+student.updateAttendance("yes"); //23
+student.updateAttendance("yes"); //24
+student.updateAttendance("yes"); //25
 
-console.log('Student', student);
-console.log('Age', student.age);
-console.log('Rating', student.averageRating);
-console.log('Visit', student.averageVisit);
-console.log('Summary', student.summary());
-
+console.log("Student", student);
+console.log("Age", student.age);
+console.log("Rating", student.averageRating);
+console.log("Visit", student.averageVisit);
+console.log("Summary", student.summary());
